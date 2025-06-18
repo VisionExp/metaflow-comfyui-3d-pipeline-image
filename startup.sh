5a61543d7b7fde267596485fb1d7dcdd7feac99d
deslancer <deslancers@gmail.com>
2025-06-18 19:38:23 +0200

    fix 3

--- startup.sh
+++ startup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Start ComfyUI
-cd /home/ComfyUI/custom_nodes/ComfyUI-Manager || { echo "Directory ComfyUI-Manager not found"; exit 1; }
+cd /home/ComfyUI/ || { echo "Directory ComfyUI not found"; exit 1; }
 nohup python main.py --listen 0.0.0.0 > /var/log/comfyui.log 2>&1 &
 echo "ComfyUI started on port 8188"
 
-# Start Jupyter Lab
-cd /home/ComfyUI || { echo "ComfyUI directory not found"; exit 1; }
 echo "Starting Jupyter Lab on port 8888"
 jupyter lab --allow-root --no-browser --ip=0.0.0.0 --port=8888
